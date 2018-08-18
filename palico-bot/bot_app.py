c1ba43a264d512acf8599b8ea94139e62b856895
Ville Kaikkonen <ville.kaikk@gmail.com>
2018-08-18 20:32:55 +0300

    Added basic initialization and config

--- palico-bot/bot_app.py
+++ palico-bot/bot_app.py
@@ -7,7 +7,33 @@ Main module for Discord palico-bot
 
 """
 
-
 import discord
 import json
 import requests
+import asyncio
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+config = load_config()
+Client = discord.Client()
+bot = Bot(command_prefix="#", description="Monster Hunter World database bot")
+
+bot.run(config["bot_token"])
+
+@bot.event
+async def on_ready():
+    get_database()
+    print("Bot ready!")
+
+
+@bot.command()
+async def pepperoni(ctx):
+    pass
+
+def get_database():
+    pass
+
+def load_config():
+    with open("config/config.json", "r") as config_file:
+        return json.load(config_file)
