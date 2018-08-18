a232947629d41d4c6ba99af4d198d31794b2b373
Ville Kaikkonen <ville.kaikk@gmail.com>
2018-08-18 21:59:31 +0300

    Added config to gitignore and added pepperonioing and rough

--- palico-bot/bot_app.py
+++ palico-bot/bot_app.py
@@ -7,33 +7,40 @@ Main module for Discord palico-bot
 
 """
 
-import discord
-import json
-import requests
 import asyncio
+import json
 
-from discord.ext.commands import Bot
+import discord
+import requests
 from discord.ext import commands
 
-config = load_config()
 Client = discord.Client()
-bot = Bot(command_prefix="#", description="Monster Hunter World database bot")
+bot = commands.Bot(command_prefix="!",
+                   description="Monster Hunter World database bot")
 
-bot.run(config["bot_token"])
 
 @bot.event
 async def on_ready():
     get_database()
     print("Bot ready!")
+    bot.say("Hi!")
 
 
-@bot.command()
+@bot.command(pass_context=True)
 async def pepperoni(ctx):
-    pass
+    user = ctx.message.author
+    await bot.say("Rip in pepperoni {}".format(user.mention))
+
 
 def get_database():
     pass
 
+
 def load_config():
-    with open("config/config.json", "r") as config_file:
+    with open("palico-bot/config/config.json", "r") as config_file:
         return json.load(config_file)
+
+
+if __name__ == "__main__":
+    config = load_config()
+    bot.run(config["bot_token"])
